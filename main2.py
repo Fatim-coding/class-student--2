e0d786203045df1565b4bad74e2fdfdd498c87cd
Fatim-coding <timabadjie12@gmail.com>
2025-05-17 15:41:51 +0000

    second commit

---
+++ main2.py
@@ -0,0 +1,7 @@
+class student:
+    grade = 7
+    school= "West African Academy of science and technology"
+    print("Hi my name is zahara badjie student of grade",grade)
+    print("West African Academy of science and technology")
+ob = student()
+     
